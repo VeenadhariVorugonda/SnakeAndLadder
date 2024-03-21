ab78fab72df71130308a7a758d63e33bdb4b7fc4
VeenadhariVorugonda <136963622+VeenadhariVorugonda@users.noreply.github.com>
2024-03-21 14:21:20 +0530

    Create Dice.java

---
+++ Codes/Dice.java
@@ -0,0 +1,11 @@
+package dice;
+
+public class Dice {
+    int MAX = 6;
+    int MIN = 1;
+
+    public int diceRoll() {
+        double d = (Math.random() * (MAX)) + MIN;
+        return (int) d;
+    }
+}
